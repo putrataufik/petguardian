83bbe4ff27625e84929e8839e524e0ff500c4064
rayhan <116495289+RayhanSipalingIt@users.noreply.github.com>
2024-12-02 09:03:44 +0800

    fiturlogin

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { auth, googleProvider } from "../api/firebaseConfig";
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import googlelogo from "../assets/google.png";
+import fblogo from "../assets/fb.png";
 
 function SignIn() {
   const [email, setEmail] = useState("");
@@ -18,6 +20,7 @@ function SignIn() {
     }
   };
 
+
   const handleGoogleSignIn = async () => {
     try {
       await signInWithPopup(auth, googleProvider);
@@ -29,91 +32,82 @@ function SignIn() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#F1F3F4] relative">
-  {/* Back Icon */}
-  <button
-    className="absolute top-4 left-4 text-gray-500 hover:text-gray-700 flex items-center"
-    onClick={() => navigate(-1)}
-  >
-    {/* Icon Back */}
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      className="w-6 h-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15 19l-7-7 7-7"
-      />
-    </svg>
-  </button>
-
-  {/* Title */}
-  <h1 className="text-3xl font-semibold text-center mb-4">PetGuardian</h1>
-
-  {/* Subtitle */}
-  <p className="text-gray-600 text-center mb-6">Login to your Account</p>
-
-  <div className="bg-[#000000] p-8 rounded-lg w-96">
-    {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-
-    {/* Input Email */}
-    <input
-      type="email"
-      placeholder="Email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      className="block w-full p-3 border rounded mb-4 focus:outline-none focus:ring focus:border-blue-300"
-    />
-
-    {/* Input Password */}
-    <input
-      type="password"
-      placeholder="Password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      className="block w-full p-3 border rounded mb-6 focus:outline-none focus:ring focus:border-blue-300"
-    />
-
-    {/* Sign In Button */}
-    <button
-      onClick={handleEmailSignIn}
-      className="w-full bg-pink-500 text-white py-2 rounded hover:bg-pink-600 transition duration-300"
-    >
-      Sign In
-    </button>
-
-    {/* Divider */}
-    <div className="flex items-center justify-center my-6">
-      <span className="w-1/4 border-t"></span>
-      <span className="mx-2 text-gray-400">- Or Sign In With -</span>
-      <span className="w-1/4 border-t"></span>
-    </div>
-
-    {/* Google Sign In */}
-    <button
-      onClick={handleGoogleSignIn}
-      className="flex items-center justify-center w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition duration-300"
-    >
-      Google
-    </button>
-
-    {/* Sign Up Link */}
-    <p className="mt-6 text-center text-gray-600">
-      Don't have an account?{" "}
-      <a
-        href="/signup"
-        className="text-blue-500 hover:underline hover:text-blue-600"
+      {/* Back Icon */}
+      <button
+        className="absolute top-4 left-4 text-gray-500 hover:text-gray-700 flex items-center"
+        onClick={() => navigate(-1)}
       >
-        Sign up
-      </a>
-    </p>
-  </div>
-</div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+        </svg>
+      </button>
 
+      <h1 className="text-3xl font-bold text-center mb-20">PetGuardian</h1>
+
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <p className="text-gray-600 text-sm text-start font-bold">Login to your Account</p>
+        {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="block w-full p-3 border rounded mb-4 focus:outline-none focus:ring focus:border-blue-300"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="block w-full p-3 border rounded mb-6 focus:outline-none focus:ring focus:border-blue-300"
+        />
+
+        <button
+          onClick={handleEmailSignIn}
+          className="w-full bg-rose-500 text-white py-2 rounded-lg hover:bg-pink-600 transition duration-300"
+        >
+          Sign In
+        </button>
+
+        <div className="flex items-center justify-center my-4">
+          <span className="w-1/4 border-t"></span>
+          <span className="mx-2 text-gray-400">- Or Sign In With -</span>
+          <span className="w-1/4 border-t"></span>
+        </div>
+
+
+        <div className="flex space-x-5 items-center justify-center w-full py-2">
+          <img
+            src={googlelogo}
+            alt="Google Icon"
+            className="w-8 h-8"
+            onClick={handleGoogleSignIn}
+          />
+          <img src={fblogo}
+           alt="fb icon"
+           className="w-8 h-8"
+            />
+        </div>
+        <p className="mt-6 text-center text-gray-600">
+          Don't have an account?{" "}
+          <a
+            href="/signup"
+            className="text-blue-500 hover:underline hover:text-blue-600"
+          >
+            Sign up
+          </a>
+        </p>
+      </div>
+    </div>
   );
 }
 
--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -2,29 +2,34 @@ import { useState } from "react";
 import { auth } from "../api/firebaseConfig";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import googlelogo from "../assets/google.png";
+import fblogo from "../assets/fb.png";
 
 function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [username, setUsername] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSignUp = async () => {
+    if (password !== confirmPassword) {
+      setError("Passwords do not match!");
+      return;
+    }
+
     try {
-      // Buat pengguna baru dengan email dan password
       const userCredential = await createUserWithEmailAndPassword(
         auth,
         email,
         password
       );
 
-      // Update displayName dengan username
       await updateProfile(userCredential.user, {
         displayName: username,
       });
 
-      // Redirect ke halaman utama atau dashboard
       navigate("/");
     } catch (err) {
       setError(err.message);
@@ -32,45 +37,92 @@ function SignUp() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded shadow-md w-80">
-        <h1 className="text-2xl font-bold mb-4 text-center">Sign Up</h1>
-        {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#F1F3F4] relative">
+      {/* Back Icon */}
+      <button
+        className="absolute top-4 left-4 text-gray-500 hover:text-gray-700 flex items-center"
+        onClick={() => navigate(-1)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+        </svg>
+      </button>
 
-        {/* Input Username */}
+      <h1 className="text-3xl font-bold text-center mb-20">PetGuardian</h1>
+
+      <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+      <p className="text-gray-600 text-start font-bold mb-4">Create your Account</p>
+        {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+
         <input
           type="text"
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="border p-2 mb-2 w-full rounded"
+          className="block w-full p-3 border rounded mb-4 focus:outline-none focus:ring focus:border-blue-300"
         />
 
-        {/* Input Email */}
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="border p-2 mb-2 w-full rounded"
+          className="block w-full p-3 border rounded mb-4 focus:outline-none focus:ring focus:border-blue-300"
         />
 
-        {/* Input Password */}
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="border p-2 mb-4 w-full rounded"
+          className="block w-full p-3 border rounded mb-4 focus:outline-none focus:ring focus:border-blue-300"
         />
 
-        {/* Tombol Sign Up */}
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          className="block w-full p-3 border rounded mb-6 focus:outline-none focus:ring focus:border-blue-300"
+        />
+
         <button
           onClick={handleSignUp}
-          className="bg-blue-500 text-white py-2 px-4 rounded w-full hover:bg-blue-600"
+          className="w-full bg-rose-500 text-white py-2 rounded-lg hover:bg-rose-600 transition duration-300"
         >
           Sign Up
         </button>
+
+        <div className="flex items-center justify-center my-4">
+          <span className="w-1/4 border-t"></span>
+          <span className="mx-2 text-gray-400">- Or Sign Up With -</span>
+          <span className="w-1/4 border-t"></span>
+        </div>
+
+        <div className="flex space-x-5 items-center justify-center w-full py-2">
+            <img src={googlelogo} alt="Google" className="w-8 h-8 " />
+        
+
+            <img src={fblogo} alt="Facebook" className="w-8 h-8" />
+         
+        </div>
+
+        <p className="mt-6 text-center text-gray-600">
+          Already have an account?{" "}
+          <a
+            href="/signin"
+            className="text-blue-500 hover:underline hover:text-blue-600"
+          >
+            Sign in
+          </a>
+        </p>
       </div>
     </div>
   );
